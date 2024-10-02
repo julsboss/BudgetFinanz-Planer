3d7999b8e916266b737e5f8c7b08eae57d15ec81
julsboss <bossauer.j@gmail.com>
2024-10-02 13:02:11 +0200

    Cashflow Interfaces implementiert

--- mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/mymonthlybudget/data/api/Cashflow.java
+++ mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/mymonthlybudget/data/api/Cashflow.java
@@ -2,4 +2,11 @@ package mosbach.dhbw.de.mymonthlybudget.data.api;
 
 public interface Cashflow {
 
+        String getType();
+        String getCategory();
+        Double getAmount();
+        String getDate();
+        String getPaymentMethod();
+        String getRepition();
+        String getComment();
 }
---
+++ mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/mymonthlybudget/data/api/CashflowManager.java
@@ -0,0 +1,10 @@
+package mosbach.dhbw.de.mymonthlybudget.data.api;
+
+import java.util.List;
+
+public interface CashflowManager {
+
+    void addCashflow(Cashflow newCashflow);
+
+    List<Cashflow> getAllCashflows();
+}
