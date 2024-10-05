4241438a7c7fb362199a898b2e38b7a06743c39a
julsboss <bossauer.j@gmail.com>
2024-10-06 00:41:54 +0200

    Homepage und Startpage hinzugefügt ( google maps klappt noch nicht )

---
+++ mosbach-tasks-web-main/public/js/Startpage.js
@@ -0,0 +1,32 @@
+/*document.getElementById('pac-input').addEventListener('input', function() {
+    const location = this.value;
+    wh_location('map', location);
+});
+
+function wh_location(my_id, my_location) {
+    const string_loc = "https://maps.google.com/maps?q=" + encodeURIComponent(my_location) + "+Geldautomat&output=embed";
+    const map = document.getElementById(my_id);
+    map.src = string_loc;
+}
+function wh_location(my_id, my_location){
+    var string_loc = "https://maps.google.com/maps?q="+ my_location +"+Geldautomat&output=embed";
+
+    var map = document.getElementById(my_id);
+    console.log(string_loc);
+    map.src = string_loc;
+
+    //map.src = map.src;
+
+    document.getElementById(my_id).src = document.getElementById(my_id).src;
+}*/
+function wh_location(my_id, my_location){
+    var string_loc = "https://maps.google.com/maps?q="+ my_location +"+Geldautomat&output=embed";
+
+    var map = document.getElementById(my_id);
+    console.log(string_loc);
+    map.src = string_loc;
+
+    //map.src = map.src;
+
+    document.getElementById(my_id).src = document.getElementById(my_id).src;
+}
