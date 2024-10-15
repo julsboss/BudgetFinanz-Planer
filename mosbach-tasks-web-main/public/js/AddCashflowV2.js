4ff5249be3624945c42965b32cf0abfec19eae01
julsboss <bossauer.j@gmail.com>
2024-10-15 16:14:43 +0200

    Aufgeräumt und Ordnerstruktur angepasst

--- mosbach-tasks-web-main/public/js/AddCashflowV2.js
+++ mosbach-tasks-web-main/public/js/AddCashflowV2.js
@@ -62,3 +62,51 @@ document.addEventListener('DOMContentLoaded', function() {
     const today = new Date().toISOString().split('T')[0];
     document.getElementById('date').value = today;
 });
+function goBack() {
+    window.location.href = '../Startpage.html';
+}
+
+$(document).ready(function() {
+    $("#submit").click(function(event) {
+        event.preventDefault(); // Verhindert das Standardverhalten des Formulars
+
+        var cashflowData = {
+            type: $("#type").val(),
+            category: $("#category").val(),
+            amount: parseFloat($("#amount").val()),
+            date: $("#date").val(),
+            paymentMethod: $("#paymentMethod").val(),
+            repetition: $("#repetition").val(),
+            comment: $("#comment").val()
+        };
+
+        // Einfacher Validierungscheck
+        if (!cashflowData.type || !cashflowData.category || isNaN(cashflowData.amount) || !cashflowData.date || !cashflowData.paymentMethod || !cashflowData.repetition) {
+            alert('Bitte alle erforderlichen Felder ausfüllen.');
+            return;
+        }
+
+        console.log("Cashflow-Daten:", cashflowData);
+
+        $.ajax({
+            url: 'https://BudgetBackend-active-lemur-qg.apps.01.cf.eu01.stackit.cloud/api/cashflow', // Stelle sicher, dass die URL korrekt ist
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            headers: {
+                'Authorization': localStorage.getItem('authToken') // Token aus dem lokalen Speicher
+            },
+            data: JSON.stringify(cashflowData),
+            success: function(data) {
+                console.log('Finanzfluss erfolgreich hinzugefügt', data);
+                alert('Finanzfluss erfolgreich hinzugefügt.');
+                // Optional: Seite neu laden oder Formular zurücksetzen
+                location.reload();
+            },
+            error: function(xhr, ajaxOptions, thrownError) {
+                console.log('Fehler: ' + xhr.status + ' ' + thrownError);
+                alert('Ein Fehler ist beim Hinzufügen der Finanzen aufgetreten. Bitte versuche es erneut.');
+            }
+        });
+    });
+});
